8702b931e254c477ff2148a0b1229a9f0fcb0417
Cy Brown <cy@cybrown.fr>
2013-07-15 13:30:37 +0200

    Untabify, added more tests and pretty printing of json database in tests

--- tests/HashtagTodo/Tests/TodoControllerTest.php
+++ tests/HashtagTodo/Tests/TodoControllerTest.php
@@ -8,6 +8,7 @@ class TodoControllerTest extends WebTestCase
 {
     public function testPost()
     {
+        $this->assertTrue($this->app['simpledb.pretty']);
         $client = $this->createClient();
         $client->request('POST', '/', array(
             "title" => "thetitle",
@@ -32,20 +33,34 @@ class TodoControllerTest extends WebTestCase
         $client = $this->createClient();
         $client->request('GET', '/' . $id);
         $this->assertTrue($client->getResponse()->isOK());
-        $jsonResponse = json_decode($client->getResponse()->getContent());
-        $this->assertEquals($jsonResponse->id, $id);
+        $jsonResponse = json_decode($client->getResponse()->getContent(), JSON_OBJECT_AS_ARRAY);
+        $this->assertArrayHasKey('id', $jsonResponse);
+        $this->assertArrayHasKey('title', $jsonResponse);
+        $this->assertArrayHasKey('description', $jsonResponse);
+        $this->assertEquals($jsonResponse['id'], $id);
+        $this->assertEquals($jsonResponse['title'], 'thetitle');
+        $this->assertEquals($jsonResponse['description'], 'thedescription');
+        $this->assertEquals(count($jsonResponse), 3);
     }
 
     /**
     * @depends testPost
     */
-    public function testGet()
+    public function testGet($id)
     {
         $client = $this->createClient();
         $client->request('GET', '/');
         $this->assertTrue($client->getResponse()->isOk());
-        $jsonResponse = json_decode($client->getResponse()->getContent());
+        $jsonResponse = json_decode($client->getResponse()->getContent(), JSON_OBJECT_AS_ARRAY);
         $this->assertTrue(is_array($jsonResponse));
+        $this->assertCount(1, $jsonResponse);
+        $this->assertArrayHasKey('id', $jsonResponse[0]);
+        $this->assertArrayHasKey('title', $jsonResponse[0]);
+        $this->assertArrayHasKey('description', $jsonResponse[0]);
+        $this->assertEquals($jsonResponse[0]['id'], $id);
+        $this->assertEquals($jsonResponse[0]['title'], 'thetitle');
+        $this->assertEquals($jsonResponse[0]['description'], 'thedescription');
+        $this->assertEquals(count($jsonResponse[0]), 3);
     }
 
     /**
@@ -59,6 +74,13 @@ class TodoControllerTest extends WebTestCase
             "description" => 'otherdescription'
         ));
         $this->assertTrue($client->getResponse()->isOk());
+        $jsonResponse = json_decode($client->getResponse()->getContent(), JSON_OBJECT_AS_ARRAY);
+        $this->assertArrayHasKey('id', $jsonResponse);
+        $this->assertArrayHasKey('title', $jsonResponse);
+        $this->assertArrayHasKey('description', $jsonResponse);
+        $this->assertEquals($jsonResponse['title'], 'othertitle');
+        $this->assertEquals($jsonResponse['description'], 'otherdescription');
+        $this->assertEquals(count($jsonResponse), 3);
     }
 
     /**
@@ -71,10 +93,23 @@ class TodoControllerTest extends WebTestCase
         $this->assertTrue($client->getResponse()->isOk());
     }
 
+    /**
+    * @depends testDelete
+    */
+    public function testGetAllEmpty()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/');
+        $this->assertTrue($client->getResponse()->isOk());
+        $jsonResponse = json_decode($client->getResponse()->getContent());
+        $this->assertEmpty($jsonResponse);
+    }
+
     public function createApplication()
     {
         $app = require __DIR__.'/../../../src/app.php';
         $app['debug'] = true;
+        $app['simpledb.pretty'] = true;
         $app['session.test'] = true;
         $app['exception_handler']->disable();
         return $app;
